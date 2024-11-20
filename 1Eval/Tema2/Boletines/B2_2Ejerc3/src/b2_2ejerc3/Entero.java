38d20940fa61bec235f2508259faa84ef83de159
rodrigosanchez <30.rodrigo.12@gmail.com>
2024-11-20 08:38:31 +0100

    correction B2_2Ejerc3

--- 1Eval/Tema2/Boletines/B2_2Ejerc3/src/b2_2ejerc3/Entero.java
+++ 1Eval/Tema2/Boletines/B2_2Ejerc3/src/b2_2ejerc3/Entero.java
@@ -2,37 +2,37 @@ package b2_2ejerc3;
 
 public class Entero {
     // atributos
-    private static int n;
+    private int n;
 
     // constructores
     public Entero() {
-        n = 0;
+        this.n = 0;
     }
 
-    public Entero(int newN) {
-        n = newN;
+    public Entero(int n) {
+        this.n = n;
     }
 
     // getters y setters
-    public void setN(int newN) {
-        n = newN;
+    public void setN(int n) {
+        this.n = n;
     }
 
     public int getN() {
-        return n;
+        return this.n;
     }
 
     // metodos
     public int add(int n1, int n2) {
-        return n1 + n2;
+        return n1 += n2;
     }
 
     public int resta(int n1, int n2) {
-        return n1 - n2;
+        return n1 -= n2;
     }
 
     public int getDoble() {
-        return 2 * n;
+        return 2 * this.n;
     }
 
     public int getTriple() {
--- 1Eval/Tema2/Boletines/B2_2Ejerc3/src/b2_2ejerc3/test.java
+++ 1Eval/Tema2/Boletines/B2_2Ejerc3/src/b2_2ejerc3/test.java
@@ -19,20 +19,27 @@ public class test {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner teclado = new Scanner(System.in);
-        int n1, n2;
+        int n1, n2, resp;
         Entero numero1 = new Entero();
 
-        System.out.print("Introduce un número entero: ");
-        n1 = teclado.nextInt();
-        numero1.setN(n1);
+        System.out.print("¿Quieres usar el número entero por defecto(1) o introducir uno distinto(2)? ");
+        resp = teclado.nextInt();
+        if (resp == 1) {
+            numero1 = new Entero();
+        } else if (resp == 2) {
+            System.out.print("Introduce un número entero: ");
+            n1 = teclado.nextInt();
+            numero1.setN(n1);
+        } else
+            System.out.println("El número introducido no es válido.");
         System.out.printf("Introduce otro número entero: ");
         n2 = teclado.nextInt();
 
-        System.out.printf("%d + %d = %d\n", n1, n2, numero1.add(n1, n2));
-        System.out.printf("%d - %d = %d\n", n1, n2, numero1.resta(n1, n2));
+        System.out.printf("%d + %d = %d\n", numero1.getN(), n2, numero1.add(numero1.getN(), n2));
+        System.out.printf("%d - %d = %d\n", numero1.getN(), n2, numero1.resta(numero1.getN(), n2));
         System.out.printf("%d * 2 = %d\n" +
                 "%d * 3 = %d\n",
-                n1, numero1.getDoble(), n1, numero1.getTriple());
+                numero1.getN(), numero1.getDoble(), numero1.getN(), numero1.getTriple());
 
         teclado.close();
     }
