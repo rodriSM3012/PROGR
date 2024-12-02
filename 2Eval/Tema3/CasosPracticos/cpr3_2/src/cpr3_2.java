8e8b617f7e9151a078541e6adedc4bd161029b9a
rodrigosanchez <30.rodrigo.12@gmail.com>
2024-12-02 10:08:24 +0100

    added cpr3_2 (for)

---
+++ 2Eval/Tema3/CasosPracticos/cpr3_2/src/cpr3_2.java
@@ -0,0 +1,8 @@
+public class cpr3_2 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++){
+            System.out.print("Hoy es lunes. ");
+            System.out.println(i);
+        }
+    }
+}
