4cd1eb339c5f97dd206f6713adbbccefa0074e24
rodrism3012 <30.rodrigo.12@gmail.com>
2024-12-16 17:07:56 +0100

    added cpr3_4

---
+++ 2Eval/Tema3/CasosPracticos/cpr3_4/src/cpr3_4.java
@@ -0,0 +1,10 @@
+public class cpr3_4 {
+    public static void main(String[] args) {
+        for (int x = 0; x < 10; x++) {
+            System.out.println("Dentro del bucle.");
+            // break;
+            // System.out.println("Nunca lo escribe.");
+        }
+        System.out.println("Tras finalizar el bucle.");
+    }
+}
