2893c57e84c12a3c6780a65de49c3d037f8b8942
rodrigosanchez <30.rodrigo.12@gmail.com>
2025-01-29 10:11:46 +0100

    add length check for string in B6_1Ejerc2

--- 2Eval/Tema6/Boletines/B6_1Ejerc2/src/B6_1Ejerc2.java
+++ 2Eval/Tema6/Boletines/B6_1Ejerc2/src/B6_1Ejerc2.java
@@ -18,6 +18,9 @@ public class B6_1Ejerc2 {
             sb.delete(0, 5);
             sb.delete((sb.length() - 2), sb.length());
             System.out.println(sb);
+            // System.out.println(cad.substring(5, cad.length() - 2));
+        } else {
+            System.out.println("La longitud de la cadena no es superior a 10.");
         }
     }
 }
