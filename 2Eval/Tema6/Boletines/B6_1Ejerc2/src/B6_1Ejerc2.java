e2afced3dd324db918771645ebf2770368d66fa6
rodrigosanchez <30.rodrigo.12@gmail.com>
2025-01-29 08:52:29 +0100

    add B6_1Ejerc2

---
+++ 2Eval/Tema6/Boletines/B6_1Ejerc2/src/B6_1Ejerc2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class B6_1Ejerc2 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String cad;
+
+        System.out.print("Introduce una cadena: ");
+        cad = teclado.nextLine();
+        cortarCadena(cad);
+
+        teclado.close();
+    }
+
+    public static void cortarCadena(String cad) {
+        StringBuilder sb = new StringBuilder(cad);
+        if (sb.length() > 10) {
+            sb.delete(0, 5);
+            sb.delete((sb.length() - 2), sb.length());
+            System.out.println(sb);
+        }
+    }
+}
