4027d9f58f248db840ab1401a56845669a0cf84f
rodrism3012 <30.rodrigo.12@gmail.com>
2025-03-12 15:03:49 +0100

    added B3_4Ejerc4: decreasing sequence from 100 to 0 subtracting 7 at each step

---
+++ 2Eval/Tema3/Boletines/B3_4Ejerc4/src/B3_4Ejerc4.java
@@ -0,0 +1,8 @@
+public class B3_4Ejerc4 {
+    public static void main(String[] args) {
+        for (int i = 100; i > 2; i -= 7) {
+            System.out.print(i + ", ");
+        }
+        System.out.print("2");
+    }
+}
